b6d270a0761c08df9d07f5f5727a03248b5e37e4
Charly <plmallorca@gmail.com>
2022-11-28 20:02:27 +0100

    Iteration#5

---
+++ ejercicio3.js
@@ -0,0 +1,15 @@
+let num = 10
+let num2 = 5
+console.log('multiplicacion', num*num2)
+let num3 = 2
+console.log('division', num/num3)
+let num4 = 15
+let num5 = 9
+console.log('resto', num4%num5)
+
+const y = 10
+const z = 5
+x = y + z
+console.log('resultado:', x)
+x = y * z
+console.log('resultado:', x)
