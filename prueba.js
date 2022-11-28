567f583e2338a25fe6421f30b75c3bface03eba7
Charly <plmallorca@gmail.com>
2022-11-28 21:22:00 +0100

    Iteration#2

---
+++ prueba.js
@@ -0,0 +1,36 @@
+let a = 10;
+let b = 12;
+a = 12
+b = 10
+
+console.log('a vale ' + a)
+console.log('b vale ' + b)
+
+//switch
+var Junio
+switch() {
+    case 1:
+        mes = Enero;
+        dias = 31
+        break;
+    case 2:
+        mes = febrero;
+        dias = 28;
+        break;
+    case 3:
+        mes = Marzo;
+        dias = 31
+        break;
+    case 4:
+        mes = Abril;
+        dias = 30
+        break;
+    case 5:
+        mes = Mayo;
+        dias = 31
+        break;
+    case 6:
+        mes = Junio;
+        dias = 30
+        break;
+}
